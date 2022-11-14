383fbf3485f2f8b150e9124a4208ad8ea7cd5f86
mayanktamrkar <mayanktamrkar786@gmail.com>
2022-11-14 08:33:57 +0530

    responsive chat and regstraion

--- registration_page/index.js
+++ registration_page/index.js
@@ -1,26 +1,41 @@
-
-function check(e){
-   
-    // e.preventDefault()
-    let fullname=document.forms["myform"]["fullname"].value
-    let email=document.forms["myform"]["email"].value
-    let password=document.forms["myform"]["password"].value
-    let cpassword=document.forms["myform"]["cpassword"].value
-    let gender=document.forms["myform"]["gender"].value
-    let country=document.forms["myform"]["country"].value
-    let address=document.forms["myform"]["address"].value
-    let terms=document.forms["myform"]["terms"].value
-    if(fullname!=="" && email!=="" && password!=="" && cpassword!=="" && gender!=="" && country!=="" && address!=="" ){
-        console.log("working")
-
+function check(e) {
+    // e.preventDefault();
+    let fullname = document.forms["myform"]["fullname"].value;
+    let email = document.forms["myform"]["email"].value;
+    let password = document.forms["myform"]["password"].value;
+    let cpassword = document.forms["myform"]["cpassword"].value;
+    let gender = document.forms["myform"]["gender"].value;
+    let country = document.forms["myform"]["country"].value;
+    let address = document.forms["myform"]["address"].value;
+    let terms = document.forms["myform"]["terms"].value;
+    if (
+        fullname !== "" &&
+        email !== "" &&
+        password !== "" &&
+        cpassword !== "" &&
+        gender !== "" &&
+        country !== "" &&
+        address !== ""
+    ) {
+        if (password !== "" && cpassword !== "") {
+            if (password === cpassword) {
+                alert("success");
+            } else {
+                alert("password and confirm password not match");
+            }
+        }
     }
-    if(password==cpassword){
-        console.log("pp")
-    }
 
-    console.log(fullname,password,cpassword,email,gender,country,address,terms)
-
+    console.log(
+        fullname,
+        password,
+        cpassword,
+        email,
+        gender,
+        country,
+        address,
+        terms
+    );
 }
 
-
-let a=document.getElementById("submit").addEventListener("click",check)
+let a = document.getElementById("submit").addEventListener("click", check);
